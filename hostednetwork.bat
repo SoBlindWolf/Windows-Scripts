99d68b76e414b2e4fac76d8f3db0752b9fe23dc2
SoBlindWolf <SoBlindWolf@users.noreply.github.com>
2016-05-23 21:35:40 -0500

    Update hostednetwork.bat

--- hostednetwork.bat
+++ hostednetwork.bat
@@ -2,7 +2,7 @@
 echo "Stopping any current Hosted Network!"
 netsh wlan stop hostednetwork
 echo "Setting up Hosted Network!"
-netsh wlan set hostednetwork mode=allow ssid=WSBlind-LBC key=TestTest#0123!
+netsh wlan set hostednetwork mode=allow ssid=HotSpotNameHere key=Pa$$w0rdH3r3
 echo "Starting Hosted Network"
 netsh wlan start hostednetwork
 pause
